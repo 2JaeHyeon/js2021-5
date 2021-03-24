7804ce8c892adb5eb2433c1061ef23f15a9d79b4
JaeHyeon2 <atom4545@naver.com>
2021-03-24 11:41:33 +0900

    수업추가내용

--- Hello.js
+++ Hello.js
@@ -1,4 +1,27 @@
-console.log("Hello World..!");
-console.log("Hello World..!");
-console.log("Hello World..!");
-console.log("Hello World..!");
+let date = new Date();
+h = date.getHours();
+
+console.log(h< 3|| h>8);
+console.log(h>= 3 && h<= 8);
+
+
+
+// (date.getHours()==0)
+if(input%2==0){
+    console.log("짝수");
+}else{
+    console.log("홀수");
+}
+
+if(date.getHours()<12){
+    console.log("오전");
+}
+console.log("오후");
+
+//Date()
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getTime());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
